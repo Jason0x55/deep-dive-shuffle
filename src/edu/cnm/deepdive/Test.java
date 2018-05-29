7b0e42c3d3f5d5477bb50350527626b7ba3f442f
Jason Rafferty <Jason0x55@gmail.com>
2018-05-28 22:05:08 -0600

    Added second test case.

--- src/edu/cnm/deepdive/Test.java
+++ src/edu/cnm/deepdive/Test.java
@@ -19,12 +19,26 @@ public class Test {
     reveal(deck);
     ArrayShuffles.shuffle(deck);
     reveal(deck);
+    
+    double[] deckDouble = createDeckDouble(100);
+    revealDouble(deckDouble);
+    ArrayShuffles.shuffle(deckDouble);
+    revealDouble(deckDouble);
   }
   
   private static int[] createDeck(int size) {
     int[] deck = new int[size];
     for (int i = 0; i < size; i++) {
       deck[i] = i; 
+    }
+    
+    return deck;
+  }
+  
+  private static double[] createDeckDouble(int size) {
+    double[] deck = new double[size];
+    for (int i = 0; i < size; i++) {
+      deck[i] = (double) i; 
     }
     
     return deck;
@@ -37,5 +51,13 @@ public class Test {
 //   System.out.println("");
     System.out.println(Arrays.toString(deck));
   }
+  
+  private static void revealDouble (double[] deck) {
+//  for (int value : deck) {
+//    System.out.print(value + " ");
+//  }
+//  System.out.println("");
+   System.out.println(Arrays.toString(deck));
+ }
 
 }
